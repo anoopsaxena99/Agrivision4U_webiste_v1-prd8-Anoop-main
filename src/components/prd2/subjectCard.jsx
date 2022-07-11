46519a0252541a41795cfabf1bae61c7519714a4
anoops <anoopsaxena99@gmail.com>
2022-07-11 20:58:22 +0530

    added link on the bottom slider of subjects in prd2

--- src/components/prd2/subjectCard.jsx
+++ src/components/prd2/subjectCard.jsx
@@ -5,44 +5,53 @@ import { FaStar } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 function SubjectCard({ onesub }) {
-  console.log("kh",onesub);
-  return (
-    <div>
-      <div className={styles.SliderContainer}>
-        <div className={styles.cardContainer}>
-          <img className={styles.img} src={Book} alt="" />
+  // console.log("kh",onesub);
 
-          <div className={styles.cardHeading}>
-            <p>""</p>
-          </div>
-          <div className={styles.cardEnrolled}>
-            <p>1.2k students</p>
-          </div>
-          <div className={styles.cardProductRating}>
-            <span className={styles.cardRating}>4.7</span>
-            <span className={styles.cardStar}>
-              <FaStar></FaStar>
-            </span>
-            <span className={styles.cardStar}>
-              <FaStar></FaStar>
-            </span>
-            <span className={styles.cardStar}>
-              <FaStar></FaStar>
-            </span>
-            <span className={styles.cardStar}>
-              <FaStar></FaStar>
-            </span>
-            <span className={styles.cardStar}>
-              <FaStar></FaStar>
-            </span>
-            <span className={styles.cardDetails}>(218248)</span>
+  return (
+    <>
+      <ViewLink to={{ pathname: `/courses/subject=${onesub}` }}>
+        <div>
+          <div className={styles.SliderContainer}>
+            <div className={styles.cardContainer}>
+              <img className={styles.img} src={Book} alt="" />
+
+              <div className={styles.cardHeading}>
+                <p>{onesub}</p>
+              </div>
+              <div className={styles.cardEnrolled}>
+                <p>1.2k students</p>
+              </div>
+              <div className={styles.cardProductRating}>
+                <span className={styles.cardRating}>4.7</span>
+                <span className={styles.cardStar}>
+                  <FaStar></FaStar>
+                </span>
+                <span className={styles.cardStar}>
+                  <FaStar></FaStar>
+                </span>
+                <span className={styles.cardStar}>
+                  <FaStar></FaStar>
+                </span>
+                <span className={styles.cardStar}>
+                  <FaStar></FaStar>
+                </span>
+                <span className={styles.cardStar}>
+                  <FaStar></FaStar>
+                </span>
+                <span className={styles.cardDetails}>(218248)</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </ViewLink>
+    </>
   );
 }
-
+const ViewLink = styled(Link)`
+  text-decoration: none;
+`;
 export default SubjectCard;
