5132654272e50bce4ff458463cef4c1ad994104e
Ajit Kumar <akres9901@gmail.com>
2022-07-15 05:29:07 +0530

    Prd6 & Prd3 integrated

--- src/components/Section/Notes/TopicCard.jsx
+++ src/components/Section/Notes/TopicCard.jsx
@@ -3,41 +3,25 @@ import subject from "./subject.jpg";
 import { useEffect, useState } from "react";
 import styles from "./lectures.module.css";
 import styled from "styled-components";
-
 import { useLocation, Link } from "react-router-dom";
 import { baseURL } from "../../../Apis";
 import Search from "../Search";
-import styled from "styled-components";
-const NotesCard = ({
-  chapterId,
-  courseId,
-  obj,
-  i,
-  pagenumber,
-  handelSub,
-  completed,
-}) => {
-  console.log("neeww", chapterId, courseId, obj, i, pagenumber);
+const NotesCard = ({obj,i,pagenumber,handelSub,completed,items})=> {
   return (
     <>
-      <div className={styles.cards}>
-        <ViewLink
-          to={{ pathname: `/course/${courseId}/${chapterId}/${obj._id}` }}
-        >
+        
+        <div className={styles.cards}  onClick={()=> {console.log(items,i)}} >
           <img src={subject} alt="topic" className={styles.img} />
 
           <div className={styles.lower}>
-            <ViewLink
-              key={i}
-              to={{
-                pathname: `/topic/${obj._id}`,
-                state: { topicsData: items, topicIndex: { i } },
-              }}
-            >
-              <div className={styles.about}>
-                <p className={styles.number}>{i + 1 + (pagenumber - 1) * 9}.</p>
-                <p className={styles.text}>{obj.name}</p>
-              </div>
+          {/* <ViewLink key ={i} to={{pathname: `/topic/${obj._id}`, state: { topicsData:items }}}> */}
+          <ViewLink key ={i} to={{pathname: `/topic/${obj._id}`, state: { topicsData:items , topicIndex:{i}}}}>
+            <div className={styles.about}>
+              <p className={styles.number}>
+                {i + 1 + (pagenumber - 1) * 9}.
+              </p>
+              <p className={styles.text}>{obj.name}</p>
+            </div>
             </ViewLink>
             <svg
               className={styles.icon}
@@ -54,15 +38,18 @@ const NotesCard = ({
             >
               <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
             </svg>
+           
           </div>
-
+       
           <p className={styles.time}>30mintutes</p>
-        </ViewLink>
-      </div>
-    </>
-  );
-};
-const ViewLink = styled(Link)`
+        </div>
+       
+        </>
+      
+      );
+        
+  }
+  const ViewLink = styled(Link)`
   text-decoration: none;
   color: black;
   font-style: auto;
@@ -70,5 +57,5 @@ const ViewLink = styled(Link)`
   &:hover {
     color: #1bbc9b;
   }
-`;
+  `;
 export default NotesCard;
--- src/components/prd6/Sidebar.js
+++ src/components/prd6/Sidebar.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import SubMenu from "./SubMenu";
 
 // import courseData from "../../data.json";
 
