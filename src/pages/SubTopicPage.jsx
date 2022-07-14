f4893f495a2789e4b70d6a82180ca210a73bbbc6
Ajit Kumar <akres9901@gmail.com>
2022-07-15 05:02:28 +0530

    Prd6 & prd3 integrated

--- src/pages/SubTopicPage.jsx
+++ src/pages/SubTopicPage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import subject from "./subject.jpg";
-
+import MainPrd6 from "./mainPrd6";
 function SubTopicPage(props){
+
   return (
     <>
-    {console.log(props)}
-
-      <img src ={subject}/> 
-     </>)
+    <MainPrd6 topicInfo={props.location.state} />
+    </>)
 }
 export default SubTopicPage;
---
+++ src/pages/mainPrd6.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import Sidebar from "../components/prd6/Sidebar";
+import TopicMaterial from "../components/prd6/TopicMaterial";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TopBreadCrumbs from "../components/prd6/TopBreadCrumbs";
+import "../App.css";
+function MainPrd6(props) {
+  // console.log(props.topicInfo);
+   const [content, setContent] = useState("");
+  const [contentType, setContentType] = useState(-1);
+  const [currentOpenTopicId, setCurrentOpenTopicId] = useState(-1);
+  const [ctype,setctype]=useState(2);
+  const handleSetctype = (data) => {
+    setctype(data);
+  };
+  const handleSetContent = (data) => {
+    setContent(data);
+  };
+
+  const handleSetContentType = (data) => {
+    setContentType(data);
+  };
+
+  const handleSetCurrentOpenTopicId=(data)=>{
+    setCurrentOpenTopicId(data)
+  }
+  return (
+    <div>
+      <Box
+        sx={{
+          flexGrow: 1,
+          margin: "40px",
+          height: `calc(100vh - 80px)`,
+          overflow: "hidden",
+        }}
+      >
+        <Grid
+          container
+          columnSpacing={2}
+          sx={{ height: "100%", overflow: "auto" }}
+        >
+          <Grid item xs={3} sx={{ height: "100%", overflow: "scroll" }}>
+            <Sidebar
+              handleSetContent={handleSetContent}
+              handleSetContentType={handleSetContentType}
+              handleSetCurrentOpenTopicId={handleSetCurrentOpenTopicId}
+              currentOpenTopicId={currentOpenTopicId}
+              topicInfo={props.topicInfo}
+              handleSetctype={handleSetctype}
+              ctype={ctype}
+            />
+          </Grid>
+          <Grid item xs={9} sx={{ height: "100%", overflow: "scroll" }}>
+            <div style={{ position: "fixed", width: "100%" }}>
+              <TopBreadCrumbs props={props}/>
+            </div>
+            <div style={{ paddingTop: "40px" }}>
+              <TopicMaterial contentType={contentType} content={content} topicId={currentOpenTopicId} ctype={ctype}/>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  )
+}
+
+export default MainPrd6
