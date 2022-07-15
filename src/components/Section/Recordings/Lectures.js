8089e2d1fed8148fffbb614db73c0913cac733e8
Ajit Kumar <akres9901@gmail.com>
2022-07-15 14:16:52 +0530

    Prd6 minor changes

--- src/components/Section/Recordings/Lectures.js
+++ src/components/Section/Recordings/Lectures.js
@@ -118,13 +118,7 @@ function Layout({ courseId, chapterId }) {
                 <div className={styles1.cardsShow}>
                   {State.map((Obj, i) => (
                     <LecturesCard
-                      courseId={courseId}
-                      chapterId={chapterId}
-                      obj={Obj}
-                      i={i}
-                      pagenumber={pagenumber}
-                      handelSub={handelSub}
-                      completed={completed}
+                    obj={Obj} i={i} pagenumber={pagenumber} handelSub={handelSub} completed={completed} items={items}
                     />
                   ))}
                 </div>
--- src/components/prd6/Sidebar.js
+++ src/components/prd6/Sidebar.js
@@ -17,8 +17,8 @@ const Sidebar = ({ handleSetContent, handleSetContentType, handleSetCurrentOpenT
   const courseData=topicInfo.topicsData;
   console.log(topicInfo);
   const handleSetSubTopics = (courseData) => {
-    console.log(topicInfo.topicIndex.i)
-    console.log(courseData.chapter.topics)
+    // console.log(topicInfo.topicIndex.i)
+    // console.log(courseData.chapter.topics)
     if (subTopics.length == 0)
       courseData.chapter.topics[topicInfo.topicIndex.i].subTopics.map((s) =>
         setSubTopics(prevState =>[...prevState, s])
