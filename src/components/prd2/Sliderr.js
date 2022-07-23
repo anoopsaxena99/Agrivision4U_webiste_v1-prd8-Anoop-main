3816ede06df4070c1f394a611c3d4e6b4314c6eb
Bijirgiri Basumatary <75986148+bijirgiriiitg@users.noreply.github.com>
2022-07-23 11:54:39 +0530

    Add files via upload

---
+++ src/components/prd2/Sliderr.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Slider from "react-slick";
+import "bootstrap/dist/css/bootstrap.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaStar } from "react-icons/fa";
+import Book from "./imageofbook.jpg";
+import styles from "./sliderr.module.css";
+import SubjectCard from "./subjectCard";
+import { useState, useEffect } from "react";
+import { baseURL } from "../../Apis";
+import { maxWidth } from "@mui/system";
+
+function ImageSlider({ subject }) {
+  let settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    initialSlide: 0,
+  };
+  console.log(subject);
+
+  const [packs, setpacks] = useState([]);
+  let name = subject;
+  useEffect(() => {
+    const fun = async (e) => {
+      const response = await fetch(`${baseURL}/package?subject=${name}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const json = await response.json();
+      if (json.success) {
+        setpacks(json.data);
+      }
+    };
+    fun();
+    // eslint-disable-next-line
+  }, [name]);
+
+  if (packs != null) console.log("packs :- ", packs);
+
+  return (
+    <>
+      {packs == 0 ? (
+        <img
+          src="/images/coming-soon.svg"
+          alt="coming-soon"
+          style={{ height: "450px", "margin-left": "0%" }}
+        ></img>
+      ) : (
+        <>
+          <Slider {...settings} style={{"margin-left": "0%" }} className={styles.slider6}>
+            {packs[0].packages.map((s) => {
+              return <SubjectCard onesub={s} />;
+            })}
+          </Slider>
+        </>
+      )}
+    </>
+  );
+}
+
+export default ImageSlider;
---
+++ src/components/prd2/tabs/subStyless.js
@@ -0,0 +1,51 @@
+import styled, {css} from "styled-components";
+
+export const TabHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-x: auto;
+  width: auto;
+  padding:50px;
+`;
+
+export const StylizedTab = styled.button`
+  color: #5f5f5f;
+  width: 100%;
+  padding: 0.05% 1%;
+  font-size: 1.5rem;
+  font-weight: 500;
+  background-color: transparent;
+  border: none;
+  border-bottom-color: #5f5f5f;
+
+  cursor: ${(p) => (p.disabled ? "default" : "pointer")};
+  ${(p) =>
+    p.active &&
+    css`
+      color: #5f5f5f;
+      font-weight: 500;
+    `}
+  ${(p) => !p.active && p.inactiveStyle}
+`;
+
+export const StyledTabPanel = styled.div`
+  display: ${(p) => (p.active ? "flex" : "none")};
+  font-size: 1rem;
+  flex-direction: column;
+  width: 100%;
+  color: black;
+  margin-top: 3vw;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const TabsHolder = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const inactiveTab = {
+  opacity: 1
+};
+export const TabSlider = styled.div`
+ `;
